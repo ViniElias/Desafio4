959dd7cdc93a6db07dd6882982ca5181f34ecb20
ViniElias <viniciuseliascr@gmail.com>
2024-06-03 19:01:00 -0300

    Add páginas do form e comentarios

--- app.py
+++ app.py
@@ -1,22 +1,69 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
 
 app = Flask("__name__")
+
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "vinielias1"
+app.config["MYSQL_DB"] = "stardew"
+mysql = MySQL(app)
+
+
+@app.route("/feedback")
+def feedback():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM feedback")
+    feedback = cur.fetchall()
+    cur.close()
+    return render_template("feedback.html", feedback=feedback)
+
+
+@app.route("/add", methods=["POST"])
+def add_fb():
+    if request.method == "POST":
+        nome = request.form["nome"]
+        email = request.form["email"]
+        comentario = request.form["comentario"]
+        cur = mysql.connection.cursor()
+        cur.execute(
+            "INSERT INTO feedback (nome, email, comentario) VALUES (%s, %s, %s)",
+            (nome, email, comentario),
+        )
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for("feedback"))
+
+
+@app.route("/delete/<int:fb_id>", methods=["POST"])
+def delete_fb(fb_id):
+    if request.method == "POST":
+        cur = mysql.connection.cursor()
+        cur.execute("DELETE FROM feedback WHERE id=%s", (fb_id,))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for("feedback"))
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/peixes")
 def peixes():
     return render_template("peixes.html")
+
 
 @app.route("/itens")
 def itens():
     return render_template("itens.html")
 
+
 @app.route("/form")
 def form():
     return render_template("form.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
